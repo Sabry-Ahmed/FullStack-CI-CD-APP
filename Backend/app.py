16e14232a4f19b0afee66b4b365a14419d554b0d
Ahmed SABRY <67513897+Sabry-Ahmed@users.noreply.github.com>
2024-02-01 10:57:23 +0100

    Adding the pytest files

--- Backend/app.py
+++ Backend/app.py
@@ -79,4 +79,4 @@ def delete_data():
         return jsonify({'message': 'Failed to delete user.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port = 5001)
